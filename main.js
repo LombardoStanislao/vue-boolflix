d5dd0a16b5175a3d3d07b997bd6933bd317f89d2
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-11-24 12:53:50 +0100

    Inizio

---
+++ main.js
@@ -0,0 +1,27 @@
+// MILESTONE 1
+// Creare un layout di base con una barra di ricerca composta da un input e un pulsante. Quando l'utente clicca sul pulsante, facciamo una chiamata all'API https://api.themoviedb.org/3/search/movie ricordandoci di passare la nostra API key e la query di ricerca, ossia il testo inserito dall'utente nell'input.
+// Con i risultati che riceviamo, visualizziamo in pagina una card per ogni film, stampando per ciascuno:
+// titolo
+// titolo in lingua originale
+// lingua originale
+// voto
+
+var app = new Vue({
+    el: '#root',
+
+        data: {
+
+        },
+
+        methods: {
+
+
+
+        },
+
+        mounted() {
+
+        },
+
+
+});
